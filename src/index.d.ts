26ebafa72c49cbbf7923dca1bcb749c463a34223
ffrostfall <80861876+ffrostflame@users.noreply.github.com>
2023-12-12 02:57:17 -0500

    update for roblox-ts support, some renamings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,16 +1,33 @@
-declare class packet<T> {
-	send: (data: T, player: Player) => void;
-	sendToAll: (data: T) => void;
-	listen: (callback: (data: T, player: Player | undefined) => void) => void;
-}
-
-interface byteNet {
-	dataTypes: {
-		string: string;
-		i8: number;
-	}
+declare class Packet<T> {
+	listen(callback: (data: T, target: Player | undefined) => void): void;
 	
-	definePacket: <_, T>(reliabilityType: "reliable" | "unreliable", structure: T) => packet<T>;
+	/**
+	 * @server
+	 */
+	sendToAll(data: T): void;
+	
+	send(data: T, target: Player): void;
 }
 
-export = byteNet;
+interface DataTypes {
+	string: string;
+	int8: number;
+	int16: number;
+	int32: number;
+	uint8: number;
+	uint16: number;
+	uint32: number;
+	
+	float32: number;
+	float64: number;
+	
+	vec3: Vector3;
+	
+	bool: boolean;
+}
+
+interface ByteNet {
+	dataTypes: DataTypes,
+	
+	definePacket: <_, T extends { [index: string]: DataTypes[keyof DataTypes] }>(reliabilityType: "reliable" | "unreliable", structure: T) => Packet<T>;
+}
